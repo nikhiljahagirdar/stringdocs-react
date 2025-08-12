c72b4bceede1abcb4bc56d2cc32fbcb5465997b5
Nikhil Jahagirdar <nikhil.jahagirdar@gmail.com>
2025-08-13 00:10:45 +0530

    Update auth.jsx
    auth api url

--- src/apis/auth.jsx
+++ src/apis/auth.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000'; // Replace with your API base URL
+const API_BASE_URL = 'https://hammerhead-app-mtxys.ondigitalocean.app'; // Replace with your API base URL
 
 // Function to register a new user
 export const registerUser = async (userData) => {
